2ab32f1a1fedfac67a40c4824eb38502e8faa142
viniciushrcs <viniciushrcs@gmail.com>
2022-08-10 07:42:27 -0300

    feat: adds vee-validate plugin to app

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
 import { createApp } from "vue";
 import { createPinia } from "pinia";
-
+import VeeValidatePlugin from "./utils/validation";
 import App from "./App.vue";
 import router from "./router";
 
@@ -11,5 +11,6 @@ const app = createApp(App);
 
 app.use(createPinia());
 app.use(router);
+app.use(VeeValidatePlugin);
 
 app.mount("#app");
