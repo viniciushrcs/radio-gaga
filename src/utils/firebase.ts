15644acbdcf8325c0e0e7d9c0b8398a8582aa3cb
viniciushrcs <viniciushrcs@gmail.com>
2022-08-12 07:13:37 -0300

    chore: config firebase

---
+++ src/utils/firebase.ts
@@ -0,0 +1,12 @@
+import firebase from "firebase/app";
+import "firebase/auth";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCa6-DY-2xUNWIrIOQLSJqlg8Rljn2NHsw",
+  authDomain: "radiogaga-1171e.firebaseapp.com",
+  projectId: "radiogaga-1171e",
+  storageBucket: "radiogaga-1171e.appspot.com",
+  appId: "1:1093928167130:web:a14c43184ba43cf6b8a7e3",
+};
+
+export default firebase.initializeApp(firebaseConfig);
