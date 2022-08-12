3fd4bceeed3667acc713e394fa8a8a7c702f6edf
viniciushrcs <viniciushrcs@gmail.com>
2022-08-12 07:09:36 -0300

    chore: add firebase

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { createApp } from "vue";
 import { createPinia } from "pinia";
 import VeeValidatePlugin from "./utils/validation";
+import "./utils/firebase";
 import App from "./App.vue";
 import router from "./router";
 
