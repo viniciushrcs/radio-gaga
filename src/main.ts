a64fecaf3c00a9d90275325d245d9aad7eb5208e
viniciushrcs <viniciushrcs@gmail.com>
2022-08-13 11:27:46 -0300

    feat: check if the user is logged in in app creation

--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,22 @@
 import { createApp } from "vue";
 import { createPinia } from "pinia";
 import VeeValidatePlugin from "./utils/validation";
-import "./utils/firebase";
+import { auth } from "./utils/firebase";
 import App from "./App.vue";
 import router from "./router";
 
 import "./assets/base.css";
 import "./assets/main.css";
 
-const app = createApp(App);
+let app: App;
+auth.onAuthStateChanged(() => {
+  if (!app) {
+    app = createApp(App);
 
-app.use(createPinia());
-app.use(router);
-app.use(VeeValidatePlugin);
+    app.use(createPinia());
+    app.use(router);
+    app.use(VeeValidatePlugin);
 
-app.mount("#app");
+    app.mount("#app");
+  }
+});
