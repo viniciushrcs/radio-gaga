06e3538e041a18932203f1469c3dba5fec02af87
viniciushrcs <viniciushrcs@gmail.com>
2022-08-16 19:08:16 -0300

    feat: adds Manage component

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,19 +1,26 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import Home from "@/views/Home.vue"
-import About from "@/views/About.vue"
+import { createRouter, createWebHistory } from "vue-router";
+import Home from "@/views/Home.vue";
+import About from "@/views/About.vue";
+import Manage from "../components/Manage.vue";
 
-const routes = [{
-  path: '/',
-  component: Home
-},
- {
-  path: '/about',
-  component: About
- }]
+const routes = [
+  {
+    path: "/",
+    component: Home,
+  },
+  {
+    path: "/about",
+    component: About,
+  },
+  {
+    path: "/manage",
+    component: Manage,
+  },
+];
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes,
-})
+});
 
-export default router
+export default router;
